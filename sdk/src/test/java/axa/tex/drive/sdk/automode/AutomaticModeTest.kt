8f2fb9d69e56775cfc125bd2a67fed62b7cc26b5
Erwan Masson <erwan.masson.emagine@axa.com>
2020-06-16 11:25:41 +0200

    [SDK] Update UT on automode

--- sdk/src/test/java/axa/tex/drive/sdk/automode/AutomaticModeTest.kt
+++ sdk/src/test/java/axa/tex/drive/sdk/automode/AutomaticModeTest.kt
@@ -1,10 +1,17 @@
 package axa.tex.drive.sdk.automode
 
 
+import android.location.Location
+import android.os.SystemClock
 import axa.tex.drive.sdk.automode.internal.tracker.SpeedFilter
 import axa.tex.drive.sdk.automode.internal.Automode
 import axa.tex.drive.sdk.acquisition.SensorService
 import axa.tex.drive.sdk.acquisition.SensorServiceFake
+import axa.tex.drive.sdk.automode.internal.states.IdleState
+import axa.tex.drive.sdk.automode.internal.states.InVehicleState
+import axa.tex.drive.sdk.automode.internal.states.TrackingState
+import axa.tex.drive.sdk.automode.internal.tracker.SPEED_MOVEMENT_THRESHOLD
+import com.google.android.gms.location.DetectedActivity
 import io.reactivex.schedulers.Schedulers
 import org.junit.Assert
 import org.junit.Test
@@ -15,22 +22,30 @@ import org.koin.dsl.module
 import org.koin.test.KoinTest
 
 class AutomaticModeTest : KoinTest {
+    @Test
+    fun testFromIdleStateToTrackingStateUsingNext() {
+        val rxScheduler = Schedulers.single()
+        val sensorServiceFake = SensorServiceFake()
+        val automode = Automode(sensorServiceFake, rxScheduler)
+
+        Assert.assertTrue(automode.getCurrentState() is IdleState)
+        automode.next()
+        Assert.assertTrue(automode.getCurrentState() is TrackingState)
+    }
 
 
     @Test
-    fun testFromIdleToInVehicleState() {
-        val myModule = module {
-            single { SpeedFilter() }
-            single { SensorServiceFake() as SensorService }
-            single { Automode(get(), Schedulers.single()) }
-        }
-        startKoin {
-            // module list
-            modules(listOf(myModule))
-        }
-        val automode: Automode by inject()
-        Assert.assertTrue(true)
-        stopKoin()
+    fun testFromTrackingStateToInVehicleStateUsingGoNext() {
+        val rxScheduler = Schedulers.single()
+        val sensorServiceFake = SensorServiceFake()
+        val automode = Automode(sensorServiceFake, rxScheduler)
+        val currentState = TrackingState(automode)
+        automode.setCurrentState(currentState)
+        Assert.assertTrue(automode.getCurrentState() is TrackingState)
+        currentState.goNext()
+        Assert.assertTrue(automode.getCurrentState() is InVehicleState)
+    }
+
     }
 
     @Test
