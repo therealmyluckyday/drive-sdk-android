f2b4a0f1b65db94a8804a5bb992374977c93e020
Erwan Masson <erwan.masson.emagine@axa.com>
2020-09-01 15:45:49 +0200

    [SDK] Update SensorServiceFake for simulate trip

--- sdk/src/main/java/axa/tex/drive/sdk/acquisition/SensorServiceFake.kt
+++ sdk/src/main/java/axa/tex/drive/sdk/acquisition/SensorServiceFake.kt
@@ -2,11 +2,16 @@ package axa.tex.drive.sdk.acquisition
 
 import android.content.Context
 import android.location.Location
+import axa.tex.drive.sdk.R
 import axa.tex.drive.sdk.automode.internal.tracker.SpeedFilter
 import axa.tex.drive.sdk.automode.internal.tracker.model.TexLocation
 import io.reactivex.Scheduler
 import axa.tex.drive.sdk.core.logger.LoggerFactory
 import io.reactivex.subjects.PublishSubject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
 
 open class SensorServiceFake : SensorService {
     private var speedFilter = SpeedFilter()
@@ -51,4 +56,57 @@ open class SensorServiceFake : SensorService {
     override fun requestForLocationPermission() {
 
     }
+
+    /**
+     * Add method to simulate trip
+     */
+
+    fun loadTrip(appContext: Context, intervalBetweenGPSPointInMilliSecond: Long) {
+        val inputStream = appContext.resources?.openRawResource(R.raw.trip_location_simulation)
+        if (inputStream == null) {
+            logger.error("ERROR File NOT FOUND", function = "loadTrip")
+        }else {
+            Thread {
+                var newTime = System.currentTimeMillis() - 86400000
+                try {
+                    logger.info("bufferedReader read", function = "loadTrip")
+                    val bufferedReader = inputStream.bufferedReader()
+                    var lineIndex = 0
+                    var line = bufferedReader.readLine()
+                    while (line != null) {
+                        newTime = sendLocationLineStringToSpeedFilter(line, newTime, intervalBetweenGPSPointInMilliSecond)
+                        line = bufferedReader.readLine()
+                        lineIndex++
+                    }
+                    logger.info("bufferedReader close", function = "loadTrip")
+                    bufferedReader.close()
+                } catch (e: IOException) {
+                    logger.error("IOException read"+e.localizedMessage, function = "loadTrip")
+                }
+            }.start()
+        }
+    }
+
+    private fun sendLocationLineStringToSpeedFilter(line: String, time: Long, intervalBetweenGPSPointInMilliSecond: Long) : Long {
+        val locationDetails = line.split(",")
+        var newLocation = Location("")
+        val latitude = locationDetails[0].toDouble()
+        newLocation.latitude = latitude
+        val longitude = locationDetails[1].toDouble()
+        newLocation.longitude = longitude
+        val accuracy = locationDetails[2].toFloat()
+        newLocation.accuracy = accuracy
+        val speed = locationDetails[3].toFloat()
+        newLocation.speed = speed // 20.F
+        val bearing = locationDetails[4].toFloat()
+        newLocation.bearing = bearing
+        val altitude = locationDetails[5].toDouble()
+        newLocation.altitude =altitude
+        val delay = locationDetails[6].toLong()
+        newLocation.time = time + delay
+        this.forceLocationChanged(newLocation)
+        Thread.sleep(intervalBetweenGPSPointInMilliSecond)
+        return newLocation.time
+    }
+
 }
