b9a9c014813284758c7cccde81e8c75a582e3458
Erwan Masson <erwan.masson.emagine@axa.com>
2020-09-10 15:32:15 +0200

    [SDK] Add thread name on LogMessage

--- sdk/src/main/java/axa/tex/drive/sdk/core/logger/LogImpl.kt
+++ sdk/src/main/java/axa/tex/drive/sdk/core/logger/LogImpl.kt
@@ -6,13 +6,15 @@ internal class LogImpl : Log {
 
     companion object {
         private val logSubject: PublishSubject<LogMessage> = PublishSubject.create()
+        private val isDebug: Boolean = false
     }
 
     internal var file: String? = null
 
     override fun print(description: String, type: LogType, function: String) {
         val className = if (file != null) file!!.splitToSequence(".").last() else "Unknown"
-        val logDetail = LogMessage(description, type, className, function)
+        val logDetail = LogMessage(description, type, className, function, Thread.currentThread().getName())
+        if (isDebug) println("["+Thread.currentThread().getName()+"]["+className+"]"+"["+function+"]"+logDetail.description)
         logSubject.onNext(logDetail)
     }
 
--- sdk/src/main/java/axa/tex/drive/sdk/core/logger/LogMessage.kt
+++ sdk/src/main/java/axa/tex/drive/sdk/core/logger/LogMessage.kt
@@ -1,4 +1,4 @@
 package axa.tex.drive.sdk.core.logger
 
-data class LogMessage(val description: String, val type: LogType, val file: String?, val function: String)
+data class LogMessage(val description: String, val type: LogType, val file: String?, val function: String, val threadName: String)
 
