9061fc66f556b50126f8ee30f6498fb071df6382
Erwan Masson <erwan.masson.emagine@axa.com>
2020-09-15 09:21:35 +0200

    [SDK] Add ThreadName on LogMessage

--- sdk/src/main/java/axa/tex/drive/sdk/core/logger/LogMessage.kt
+++ sdk/src/main/java/axa/tex/drive/sdk/core/logger/LogMessage.kt
@@ -1,4 +1,8 @@
 package axa.tex.drive.sdk.core.logger
 
-data class LogMessage(val description: String, val type: LogType, val file: String?, val function: String, val threadName: String)
+data class LogMessage(val description: String, val type: LogType, val file: String?, val function: String, val threadName: String) {
+    override fun toString(): String {
+        return "["+threadName+"]["+file+"]"+"["+function+"]["+type+"]"+description
+    }
+}
 
