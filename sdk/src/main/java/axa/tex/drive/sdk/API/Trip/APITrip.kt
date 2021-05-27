989edd32964d45592fca2650746431fcfd64026c
Erwan Masson <erwan.masson.emagine@axa.com>
2021-05-27 14:10:23 +0200

    Fix uid problem

--- sdk/src/main/java/axa/tex/drive/sdk/API/Trip/APITrip.kt
+++ sdk/src/main/java/axa/tex/drive/sdk/API/Trip/APITrip.kt
@@ -13,18 +13,17 @@ import axa.tex.drive.sdk.core.logger.LoggerFactory
 internal class APITrip : KoinComponentCallbacks {
     private val workerManager: WorkManager
     private val LOGGER = LoggerFactory().getLogger(this::class.java.name).logger
-    private val uid: String
+
     constructor(context: Context) {
         this.workerManager = WorkManager.getInstance(context)
-        this.uid = DeviceInfo.getUid(context)
     }
     var isRetrievingScoreAutomatically: Boolean = false
 
     fun sendTrip(tripChunk: TripChunk) {
-        LOGGER.info("SENDING PACKET DATA FOR WORKER MANAGER ${tripChunk.data(isRetrievingScoreAutomatically, uid)} tripChunk.isLast: ${tripChunk.isLast} ", function = "sendTrip")
+        LOGGER.info("SENDING PACKET DATA FOR WORKER MANAGER ${tripChunk.data(isRetrievingScoreAutomatically, tripChunk.tripInfos.uid)} tripChunk.isLast: ${tripChunk.isLast} ", function = "sendTrip")
         val fixUploadWork: OneTimeWorkRequest = OneTimeWorkRequest
                 .Builder(if (tripChunk.isLast) LastFixWorker::class.java else FixWorker::class.java)
-                .setInputData(tripChunk.data(isRetrievingScoreAutomatically, uid))
+                .setInputData(tripChunk.data(isRetrievingScoreAutomatically, tripChunk.tripInfos.uid))
                 .addTag((tripChunk.tripInfos.tripId).toString())
                 .setConstraints(Constraints.Builder()
                         .setRequiredNetworkType(NetworkType.CONNECTED)
