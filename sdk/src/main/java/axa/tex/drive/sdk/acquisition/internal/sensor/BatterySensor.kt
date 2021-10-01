b531765a9bbb22558fe60d3d8f1908a2d404b3f2
Erwan Masson <erwan.masson@openfleet.com>
2021-10-01 12:00:26 +0200

    Update BatterySensor if intent null do nothing

--- sdk/src/main/java/axa/tex/drive/sdk/acquisition/internal/sensor/BatterySensor.kt
+++ sdk/src/main/java/axa/tex/drive/sdk/acquisition/internal/sensor/BatterySensor.kt
@@ -66,7 +66,7 @@ internal class BatterySensor : TexSensor, BroadcastReceiver {
 
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action != Intent.ACTION_BATTERY_CHANGED) {
+        if (intent == null || intent?.action != Intent.ACTION_BATTERY_CHANGED) {
             return
         }
         // Battery level
